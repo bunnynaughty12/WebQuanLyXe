4dca967d24dc2fd3eea2c3e64207d32c09e58135
bunnynaughty12 <bunnynaughty12@gmail.com>
2024-03-03 20:35:18 +0700

    test

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyBaiGiuXe.Models;
+using System.Diagnostics;
+
+namespace QuanLyBaiGiuXe.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JD1MVM7\\SQLEXPRESS; Database=QuanLyXe; MultipleActiveResultSets = True; Trusted_connection = True; TrustServerCertificate = True;Integrated Security=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TheGiuXe>()
+                .HasOne<BaiGiuXe>(s => s.BaiGiuXe)
+                .WithMany(g => g.TheGiuXes)
+                .HasForeignKey(s => s.MaBaiGiuXe);
+        }
+
+        public DbSet<BaiGiuXe> BaiGiuXes { get; set; }
+        public DbSet<TheGiuXe> TheGiuXes { get; set; }
+    }
+}
---
+++ Models/TheGiuXe.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyBaiGiuXe.Models
+{
+    public class TheGiuXe
+    {
+        [Key]
+        public int MaTheGiuXe { get; set; }
+        public decimal Gia { get; set; }
+        public DateTime ThoiGian { get; set; }
+        public int MaBaiGiuXe { get; set; }
+
+        public BaiGiuXe BaiGiuXe { get; set; }
+    }
+}
